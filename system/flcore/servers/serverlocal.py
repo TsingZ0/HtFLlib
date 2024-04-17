d22021bd7d3b5e10deeb5ff08686804407bad2fa
Tsing <2719584131@qq.com>
2024-04-17 22:17:32 +0800

    Update serverlocal.py

--- system/flcore/servers/serverlocal.py
+++ system/flcore/servers/serverlocal.py
@@ -1,3 +1,4 @@
+import time
 from flcore.clients.clientlocal import clientLocal
 from flcore.servers.serverbase import Server
 from threading import Thread
@@ -15,10 +16,12 @@ class Local(Server):
         print("Finished creating server and clients.")
         
         # self.load_model()
+        self.Budget = []
 
 
     def train(self):
         for i in range(self.global_rounds+1):
+            s_t = time.time()
             self.selected_clients = self.select_clients()
 
             if i%self.eval_gap == 0:
@@ -35,6 +38,9 @@ class Local(Server):
             # [t.start() for t in threads]
             # [t.join() for t in threads]
 
+            self.Budget.append(time.time() - s_t)
+            print('-'*25, 'time cost', '-'*25, self.Budget[-1])
+
             if self.auto_break and self.check_done(acc_lss=[self.rs_test_acc], top_cnt=self.top_cnt):
                 break
 
@@ -43,5 +49,7 @@ class Local(Server):
         # self.print_(max(self.rs_test_acc), max(
         #     self.rs_train_acc), min(self.rs_train_loss))
         print(max(self.rs_test_acc))
+        print("\nAverage time cost per round.")
+        print(sum(self.Budget[1:])/len(self.Budget[1:]))
 
         self.save_results()
