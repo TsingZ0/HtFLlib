c7784663dbed3d71edd5047df997fd8599acd96f
Tsing <2719584131@qq.com>
2024-11-26 11:24:20 +0800

    Update main.py

--- system/main.py
+++ system/main.py
@@ -61,6 +61,15 @@ def run(args):
                 'torchvision.models.resnet18(pretrained=False, num_classes=args.num_classes)'
             ]
 
+        elif args.model_family == "HtFE5":
+            args.models = [
+                'torchvision.models.googlenet(pretrained=False, aux_logits=False, num_classes=args.num_classes)', 
+                'mobilenet_v2(pretrained=False, num_classes=args.num_classes)', 
+                'torchvision.models.resnet18(pretrained=False, num_classes=args.num_classes)',
+                'torchvision.models.resnet34(pretrained=False, num_classes=args.num_classes)', 
+                'torchvision.models.resnet50(pretrained=False, num_classes=args.num_classes)', 
+            ]
+
         elif args.model_family == "HtFE8":
             args.models = [
                 'FedAvgCNN(in_features=3, num_classes=args.num_classes, dim=1600)', 
