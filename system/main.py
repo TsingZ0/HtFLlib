290d4b41b17824a0363cf65dde2e9164d16d4917
Tsing <2719584131@qq.com>
2025-06-11 09:43:03 +0800

    Update main.py

--- system/main.py
+++ system/main.py
@@ -361,7 +361,8 @@ if __name__ == "__main__":
     parser.add_argument('-vs', "--vocab_size", type=int, default=80, 
                         help="Set this for text tasks. 80 for Shakespeare. 32000 for AG_News and SogouNews.")
     parser.add_argument('-ml', "--max_len", type=int, default=200)
-    parser.add_argument('-mfn', "--models_folder_name", type=str, default='')
+    parser.add_argument('-mfn', "--models_folder_name", type=str, default='',
+                        help="The folder of pre-trained models")
     parser.add_argument('-fs', "--few_shot", type=int, default=0)
     # practical
     parser.add_argument('-cdr', "--client_drop_rate", type=float, default=0.0,
